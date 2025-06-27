60d659fd66830afb209a7fdeeb61c8fda988b5b4
Tanuj Chandola <119155750+TanujChandola@users.noreply.github.com>
2025-06-27 08:12:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-   
+    mode === 'development'
   ].filter(Boolean),
   resolve: {
     alias: {
